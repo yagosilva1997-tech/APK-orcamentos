6f10214b06d9082e1b8850abdec1b255b7e04d45
yagosilva1997-tech <yagosilva1997@gmail.com>
2026-01-18 18:01:42 -0300

    Primeira versão

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
 });
